584df9d718e87464fb224b2f3e43a1b18d43900c
luobosi <luobosi@2dfire.com>
2018-07-12 14:22:46 +0800

    压缩map 试水

--- src/zipmap.h
+++ src/zipmap.h
@@ -35,6 +35,7 @@
 #ifndef _ZIPMAP_H
 #define _ZIPMAP_H
 
+//
 unsigned char *zipmapNew(void);
 unsigned char *zipmapSet(unsigned char *zm, unsigned char *key, unsigned int klen, unsigned char *val, unsigned int vlen, int *update);
 unsigned char *zipmapDel(unsigned char *zm, unsigned char *key, unsigned int klen, int *deleted);
