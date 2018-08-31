6344ce1e19729aa68673d01fbc0a335b0c0f82f8
luobosi <luobosi@2dfire.com>
2018-08-31 15:55:03 +0800

    Redis Stream 结构体学习

--- src/stream.h
+++ src/stream.h
@@ -4,19 +4,32 @@
 #include "rax.h"
 #include "listpack.h"
 
-/* Stream item ID: a 128 bit number composed of a milliseconds time and
+/*
+ * Stream item ID: a 128 bit number composed of a milliseconds time and
  * a sequence counter. IDs generated in the same millisecond (or in a past
  * millisecond if the clock jumped backward) will use the millisecond time
- * of the latest generated ID and an incremented sequence. */
+ * of the latest generated ID and an incremented sequence.
+ *
+ * redis stream 的消息id 比较特殊，是一个timestamp-sequencenum的结构，基本上可以理解为先比较时间戳，后比较序列号了。
+ */
 typedef struct streamID {
+    // 时间戳
     uint64_t ms;        /* Unix time in milliseconds. */
+    // 序列号
     uint64_t seq;       /* Sequence number. */
 } streamID;
 
+/**
+ * stream 对象
+ */
 typedef struct stream {
+    //rax树存储真正的消息内容
     rax *rax;               /* The radix tree holding the stream. */
+    //这个 stream 的消息长度
     uint64_t length;        /* Number of elements inside this stream. */
+    //当前stream的最后一个id
     streamID last_id;       /* Zero if there are yet no items. */
+    //这个stream有哪些group, group里面会存储对应的有哪些consumers
     rax *cgroups;           /* Consumer groups dictionary: name -> streamCG */
 } stream;
 
@@ -47,28 +60,48 @@ typedef struct streamIterator {
     unsigned char value_buf[LP_INTBUF_SIZE];
 } streamIterator;
 
-/* Consumer group. */
+/*
+ * Consumer group.
+ */
 typedef struct streamCG {
+    /**
+     * last_id 每个组的消费者共享一个 last_id 代表这个消费组到了什么位置，
+     * 每次投递后会更新这个 group。当然是必须指定 group 的时候会更新
+     */
     streamID last_id;       /* Last delivered (not acknowledged) ID for this
                                group. Consumers that will just ask for more
                                messages will served with IDs > than this. */
+    /**
+     * pel 已经发送给客户端，但是还没有收到 xack 的消息都存储在 pel 树里面， pending entries list
+     *
+     */
     rax *pel;               /* Pending entries list. This is a radix tree that
                                has every message delivered to consumers (without
                                the NOACK option) that was yet not acknowledged
                                as processed. The key of the radix tree is the
                                ID as a 64 bit big endian number, while the
                                associated value is a streamNACK structure.*/
+    /**
+     * 存储了我这个组有哪些消费者
+     */
     rax *consumers;         /* A radix tree representing the consumers by name
                                and their associated representation in the form
                                of streamConsumer structures. */
 } streamCG;
 
-/* A specific consumer in a consumer group.  */
+/*
+ * A specific consumer in a consumer group.
+ */
 typedef struct streamConsumer {
     mstime_t seen_time;         /* Last time this consumer was active. */
+    // 消费者名称
     sds name;                   /* Consumer name. This is how the consumer
                                    will be identified in the consumer group
                                    protocol. Case sensitive. */
+    /**
+     * 待ACK的消息列表，注意这个功能 streamCG 里面的实际上指向一个。
+     * 也就是说一条消息，两个地方都会记住
+     */
     rax *pel;                   /* Consumer specific pending entries list: all
                                    the pending messages delivered to this
                                    consumer not yet acknowledged. Keys are
